4f17e0752ab5af72b0e456c81e515a4727073ba7
diomaxer <68566883+diomaxer@users.noreply.github.com>
2022-08-20 22:01:30 +0300

    class Parser

--- curse_parser.py
+++ curse_parser.py
@@ -1,45 +1,69 @@
+import datetime
 import requests
+
+from typing import List
+from fastapi import HTTPException
+from starlette import status
 from bs4 import BeautifulSoup
 
-url = 'https://www.cbr.ru/scripts/XML_daily.asp'
+from database.pydantic_models import Convert, Currency, CountAmount
 
 
-async def get_url() -> BeautifulSoup:
+async def get_url(date=None) -> BeautifulSoup:
+    if date:
+        url = f'http://www.cbr.ru/scripts/XML_daily.asp?date_req={date.strftime("%d/%m/%Y")}'
+    else:
+        url = 'https://www.cbr.ru/scripts/XML_daily.asp'
     r = requests.get(url)
     encoding = r.encoding if 'charset' in r.headers.get('content-type', '').lower() else None
     soup = BeautifulSoup(r.content, 'xml', from_encoding=encoding)
     return soup
 
 
-async def get_curse():
-    currency_info = {}
-    soup = await get_url()
-    currency = soup.find_all('Valute')
-    for elem in currency:
-        currency_info[elem.find('NumCode').text] = {
-            'char_code': elem.find('CharCode').text,
-            'name': elem.find('Name').text,
-            'value': elem.find('Value').text
-        }
-    return currency_info
+class Parser:
+    @staticmethod
+    async def get_all_courses(date=None) -> List[Currency]:
+        if date and date < datetime.date(1993, 1, 1):
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="data available after 1993-01-01")
+        currency_info = []
+        soup = await get_url(date=date)
+        currency = soup.find_all('Valute')
+        for elem in currency:
+            currency_info.append(
+                Currency(
+                    num_code=elem.find('NumCode').text,
+                    char_code=elem.find('CharCode').text,
+                    name=elem.find('Name').text,
+                    value=float(elem.find('Value').text.replace(',', '.'))
+
+                )
+            )
+        return currency_info
+
+    @staticmethod
+    async def get_currencies(convert: Convert) -> CountAmount:
+        answer = {}
+
+        if convert.from_char_code == 'RUB':
+            answer['sum'] = 1
+        if convert.to_char_code == 'RUB':
+            answer['sum2'] = 1
 
 
-async def convert_valute(valute1, valute2):
-    answer = {}
+        soup = await get_url(date=convert.date)
+        currencies = soup.find_all('Valute')
 
-    if valute1 == 'RUB':
-        answer['v1'] = 1
-    if valute2 == 'RUB':
-        answer['v2'] = 1
+        for currency in currencies:
+            if currency.find('CharCode').text == convert.from_char_code:
+                answer['sum'] = float(currency.find('Value').text.replace(',', '.')) / int(currency.find('Nominal').text)
+            if currency.find('CharCode').text == convert.to_char_code:
+                answer['sum2'] = float(currency.find('Value').text.replace(',', '.')) / int(currency.find('Nominal').text)
+            if len(answer) == 2:
+                return CountAmount(**answer)
 
-    soup = await get_url()
-    currency = soup.find_all('Valute')
+        print(answer)
 
-    for elem in currency:
-        if elem.find('CharCode').text == valute1:
-            answer['v1'] = float(elem.find('Value').text.replace(',', '.')) / int(elem.find('Nominal').text)
-        if elem.find('CharCode').text == valute2:
-            answer['v2'] = float(elem.find('Value').text.replace(',', '.')) / int(elem.find('Nominal').text)
-        if len(answer) == 2:
-            return answer
-    return answer
+        missing_char_code = convert.from_char_code if 'sum2' in answer.keys() else convert.to_char_code
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"no data for this char code '{missing_char_code}'")
