6bc79c65ade841df6ebc31fa1e8247000ba86236
diomaxer <68566883+diomaxer@users.noreply.github.com>
2022-08-20 22:02:59 +0300

    Currency, Amount, CountAmount, validator in Convert

--- database/pydantic_models.py
+++ database/pydantic_models.py
@@ -1,3 +1,6 @@
+from datetime import date
+from fastapi import HTTPException
+from starlette import status
 from pydantic import BaseModel, validator
 
 
@@ -5,7 +8,31 @@ class Convert(BaseModel):
     sum: float
     from_char_code: str
     to_char_code: str
+    date: date | None
 
     @validator('from_char_code', 'to_char_code')
     def upper_char_code(cls, v):
         return v.upper()
+
+    @validator('date')
+    def check_year(cls, v):
+        if v < date(1997, 1, 1):
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="data available after 1997-01-01")
+        if v > date.today():
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="date can't be greater then today")
+        return v
+
+
+class Currency(BaseModel):
+    num_code: int
+    char_code: str
+    name: str
+    value: float
+
+
+class Amount(BaseModel):
+    sum: float
+
+
+class CountAmount(Amount):
+    sum2: float
